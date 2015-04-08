f361ed72e34609ff2a0d7c7a5884eabf7259dad4
Hallvard Traetteberg <hal@idi.ntnu.no>
2015-04-08 22:46:56 +0200

    Fjernet metode for oppdatering av del av rutenett.

--- games/src/games/IUpdateable.java
+++ games/src/games/IUpdateable.java
@@ -2,6 +2,5 @@ package games;
 
 public interface IUpdateable {
 	public void updateState(boolean fullUpdate);
-	public void updateState(int x1, int y1, int x2, int y2);
 	public void updateState(String text);
 }

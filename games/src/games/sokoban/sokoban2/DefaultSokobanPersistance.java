1ddc37fce8f4cc3c984e60844fc2f837124b7ba8
Hallvard Traetteberg <hal@idi.ntnu.no>
2015-04-08 22:54:49 +0200

    Removed unused import.

--- games/src/games/sokoban/sokoban2/DefaultSokobanPersistance.java
+++ games/src/games/sokoban/sokoban2/DefaultSokobanPersistance.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class DefaultSokobanPersistance implements ISokobanPersistance {
